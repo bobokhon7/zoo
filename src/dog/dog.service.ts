e470c2a4f2b168f53dec953d1d83d68bcc4b7dce
Bobokhon <rboboxon6055@gmail.com>
2024-05-06 12:43:40 +0900

    BRR: Intial Set up

---
+++ src/dog/dog.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class DogService {
+  public getHello(): string {
+    return 'woow';
+  }
+  public intoduce(): string {
+    return 'My name is rex';
+  }
+}
